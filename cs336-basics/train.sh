50bb8484e3848108d7b024389923985f37bd00bc
yusheng-ma <koala34025@gmail.com>
2025-07-01 20:33:11 +0800

    do train

--- cs336-basics/train.sh
+++ cs336-basics/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-uv run torchrun --standalone --nproc_per_node=4 \
+uv run torchrun --standalone --nproc_per_node=2 \
     scripts/train.py --config-name=experiment/your_data \
     +training.save_checkpoints=True \
     +training.train_batch_size=32 \
-    +training.gradient_accumulation_steps=2
+    +training.gradient_accumulation_steps=4
